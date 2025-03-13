0ab06e1d6e562f6b11e950f92af3ef7e4030fcb7
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-13 18:31:56 +0100

    create vkPipeline

---
+++ VulkanTest/Vulkan/vk_pipeline.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace vk {
+
+	class VkPipeline {
+
+	public:
+		VkPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+
+	private:
+		
+		static std::vector<char> readFile(const std::string& filepath);
+		void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+	};
+
+}
