2dc5c7fdd1bff546d489e7d6e885a2eb9cddb167
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-13 15:41:50 +0100

    create app header

--- VulkanTest/App/app.h
+++ VulkanTest/App/app.h
@@ -7,7 +7,11 @@ namespace vk {
 	class App {
 
 	public:
+<<<<<<< HEAD
 		static constexpr int WIDTH = 1200;
+=======
+		static constexpr int WIDTH = 1600;
+>>>>>>> ae210e3 (create app header)
 		static constexpr int HEIGHT = 800;
 
 		void run();
