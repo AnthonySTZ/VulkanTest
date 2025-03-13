0f46934c382ac09534560b883ec3345caf5be77e
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-13 15:49:51 +0100

    change app size

--- VulkanTest/App/app.h
+++ VulkanTest/App/app.h
@@ -7,11 +7,7 @@ namespace vk {
 	class App {
 
 	public:
-<<<<<<< HEAD
 		static constexpr int WIDTH = 1200;
-=======
-		static constexpr int WIDTH = 1600;
->>>>>>> ae210e3 (create app header)
 		static constexpr int HEIGHT = 800;
 
 		void run();
