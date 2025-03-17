744ea4b2c97ac3067886b8e2ac436384a9128103
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-03-17 10:13:33 +0100

    vertex model functions

--- VulkanTest/Vulkan/hdn_model.h
+++ VulkanTest/Vulkan/hdn_model.h
@@ -2,16 +2,36 @@
 
 #include "hdn_device.h"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
 namespace hdn {
 
 	class HdnModel {
 	public:
-		HdnModel();
+
+		struct Vertex {
+			glm::vec2 position;
+
+			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+		};
+
+		HdnModel(HdnDevice& device, const std::vector<Vertex>& vertices);
 		~HdnModel();
 
 		HdnModel(const HdnModel&) = delete;
 		HdnModel& operator=(const HdnModel&) = delete;
+
+		void bind(VkCommandBuffer commandBuffer);
+		void draw(VkCommandBuffer commandBuffer);
+
 	private:
+		void createVertexBuffers(const std::vector<Vertex>& vertices);
+
 		HdnDevice& hdnDevice;
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
