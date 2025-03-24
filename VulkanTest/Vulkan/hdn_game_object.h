54f406959228d94ac60069647e8cea456335b70b
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-24 14:58:37 +0100

    init gameObject

--- VulkanTest/Vulkan/hdn_game_object.h
+++ VulkanTest/Vulkan/hdn_game_object.h
@@ -4,7 +4,23 @@
 namespace hdn {
 
 	class HdnGameObject {
-		
-	};
+	public:
+		using id_t = unsigned int;
 
+		static HdnGameObject createGameObject() {
+			static id_t currentId = 0;
+			return HdnGameObject(currentId++);
+		};
+
+		HdnGameObject(const HdnGameObject&) = delete;
+		HdnGameObject& operator=(const HdnGameObject&) = delete;
+		HdnGameObject(HdnGameObject&&) = default;
+		HdnGameObject& operator=(HdnGameObject&&) = default;
+
+		id_t getId() const { return id; };
+
+	private:
+		HdnGameObject(id_t objId) : id{ objId } {};
+		id_t id;
+	};
 }
