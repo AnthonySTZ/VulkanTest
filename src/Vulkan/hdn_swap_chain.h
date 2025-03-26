abf25721c0461838d8808b521ff4b06a1b5eb15a
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-26 22:57:46 +0100

    add compareSwapFormats to swapChain header

--- src/Vulkan/hdn_swap_chain.h
+++ src/Vulkan/hdn_swap_chain.h
@@ -40,6 +40,11 @@ namespace hdn {
         VkResult acquireNextImage(uint32_t* imageIndex);
         VkResult submitCommandBuffers(const VkCommandBuffer* buffers, uint32_t* imageIndex);
 
+        bool compareSwapFormats(const HdnSwapChain &swapChain) const {
+            return swapChain.swapChainDepthFormat == swapChainDepthFormat && 
+                   swapChain.swapChainImageFormat == swapChainImageFormat;
+        }
+
     private:
         void init();
 
@@ -58,6 +63,7 @@ namespace hdn {
         VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
         VkFormat swapChainImageFormat;
+        VkFormat swapChainDepthFormat;
         VkExtent2D swapChainExtent;
 
         std::vector<VkFramebuffer> swapChainFramebuffers;
