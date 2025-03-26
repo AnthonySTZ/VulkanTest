b28c6cde80ef84828bf34ad87a2d22d138250998
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-26 22:48:53 +0100

    create simple render system

---
+++ src/Vulkan/simple_render_system.cpp
@@ -0,0 +1,87 @@
+#include "simple_render_system.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include <stdexcept>
+#include <array>
+#include <iostream>
+
+namespace hdn {
+
+	struct SimplePushConstantData {
+		glm::mat2 tranform{ 1.0f }; // Identity matrix
+		glm::vec2 offset;
+		alignas(16) glm::vec3 color;
+	};
+
+	SimpleRenderSystem* SimpleRenderSystem::appPointer = nullptr;
+
+	SimpleRenderSystem::SimpleRenderSystem(HdnDevice &device, VkRenderPass renderPass) : hdnDevice{device}
+	{
+		createPipelineLayout();
+		createPipeline(renderPass);
+	}
+
+	SimpleRenderSystem::~SimpleRenderSystem()
+	{
+		vkDestroyPipelineLayout(hdnDevice.device(), pipelineLayout, nullptr);
+	}
+
+	void SimpleRenderSystem::createPipelineLayout()
+	{
+		VkPushConstantRange pushConstantRange{};
+		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+		pushConstantRange.offset = 0;
+		pushConstantRange.size = sizeof(SimplePushConstantData);
+
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 0;
+		pipelineLayoutInfo.pSetLayouts = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount= 1;
+		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
+		if (vkCreatePipelineLayout(hdnDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create PipelineLayout!");
+		}
+
+	}
+
+	void SimpleRenderSystem::createPipeline(VkRenderPass renderPass)
+	{
+		assert(pipelineLayout != nullptr && "Cannot create Pipeline before pipelineLayout!");
+
+		PipelineConfigInfo pipelineConfig{};
+		HdnPipeline::defaultPipelineConfigInfo(pipelineConfig);
+		pipelineConfig.renderPass = renderPass;
+		pipelineConfig.pipelineLayout = pipelineLayout;
+		std::cout << "Loading Shaders..\n";
+		hdnPipeline = std::make_unique<HdnPipeline>(
+			hdnDevice,
+			"Shaders/vertShader.spv",
+			"Shaders/fragShader.spv",
+			pipelineConfig
+		);
+		std::cout << "Shaders loaded\n";
+	}
+
+	void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<HdnGameObject> &gameObjects)
+	{
+		hdnPipeline->bind(commandBuffer);
+
+		for (auto& obj : gameObjects) {
+			SimplePushConstantData push{};
+			push.offset = obj.transform2d.translation;
+			push.color = obj.color;
+			push.tranform = obj.transform2d.mat2();
+			vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0,
+				sizeof(SimplePushConstantData), &push);
+
+			obj.model->bind(commandBuffer);
+			obj.model->draw(commandBuffer);
+		}
+	}
+
+}
---
+++ src/Vulkan/simple_render_system.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "hdn_pipeline.h"
+#include "hdn_game_object.h"
+
+#include <memory>
+#include <vector>
+namespace hdn {
+
+	class SimpleRenderSystem {
+
+	public:
+		static SimpleRenderSystem* appPointer;
+
+		SimpleRenderSystem(HdnDevice &device, VkRenderPass renderPass);
+		~SimpleRenderSystem();
+
+		SimpleRenderSystem(const SimpleRenderSystem&) = delete;
+		SimpleRenderSystem& operator=(const SimpleRenderSystem&) = delete;
+
+        void renderGameObjects(VkCommandBuffer commandBuffer, std::vector<HdnGameObject> &gameObjects);
+
+	private:
+		void createPipelineLayout();
+		void createPipeline(VkRenderPass renderPass);
+
+		HdnDevice &hdnDevice;
+
+		std::unique_ptr<HdnPipeline> hdnPipeline;
+		VkPipelineLayout pipelineLayout;
+	};
+
+}
