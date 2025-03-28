cb683a10ebf1ec1008684c407a1107633d692384
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-28 11:15:15 +0100

    modify right dir

--- src/Vulkan/keyboard_movement_controller.cpp
+++ src/Vulkan/keyboard_movement_controller.cpp
@@ -7,7 +7,7 @@ namespace hdn {
         glm::vec3 rotate{.0f};
         if (glfwGetKey(window, keys.lookRight) == GLFW_PRESS) rotate.y += 1.f;
         if (glfwGetKey(window, keys.lookLeft) == GLFW_PRESS) rotate.y -= 1.f;
-        if (glfwGetKey(window, keys.lookUp) == GLFW_PRESS) rotate.x -= 1.f;
+        if (glfwGetKey(window, keys.lookUp) == GLFW_PRESS) rotate.x += 1.f;
         if (glfwGetKey(window, keys.lookDown) == GLFW_PRESS) rotate.x -= 1.f;
 
         if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon()){
@@ -15,11 +15,11 @@ namespace hdn {
         }
 
         gameObject.transform.rotation.x = glm::clamp(gameObject.transform.rotation.x, -1.5f, 1.5f);
-        gameObject.transform.rotation.y = glm::mod(gameObject.transform.rotation.y, glm::two_over_pi<float>());
+        gameObject.transform.rotation.y = glm::mod(gameObject.transform.rotation.y, glm::two_pi<float>());
 
         float yaw = gameObject.transform.rotation.y;
         const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
-        const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
+        const glm::vec3 rightDir{glm::cross(glm::vec3{0.f, 1.f, 0.f}, forwardDir)};
         const glm::vec3 upDir{.0f, -1.f, .0f};
 
         glm::vec3 moveDir{0.f};
