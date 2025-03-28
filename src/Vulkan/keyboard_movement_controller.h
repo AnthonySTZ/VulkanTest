a035470945275a20361ed9180646847570230de6
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-28 10:55:48 +0100

    create keyboard header

---
+++ src/Vulkan/keyboard_movement_controller.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "hdn_game_object.h"
+#include "hdn_window.h"
+
+namespace hdn{
+
+    class KeyboardMovementController{
+
+    public:
+        struct KeyMappings {
+            int moveForward = GLFW_KEY_Z;
+            int moveLeft = GLFW_KEY_Q;
+            int moveBackward = GLFW_KEY_S;
+            int moveRight = GLFW_KEY_D;
+            int moveUp = GLFW_KEY_E;
+            int moveDown = GLFW_KEY_A;
+            int lookLeft = GLFW_KEY_LEFT;
+            int lookRight = GLFW_KEY_RIGHT;
+            int lookUp = GLFW_KEY_UP;
+            int lookDown = GLFW_KEY_DOWN;
+        };
+
+        void moveInPlaneXZ();
+
+        KeyMappings keys{};
+        float moveSpeed{3.f};
+        float lookSpeed{1.5f};
+
+    };
+
+}
