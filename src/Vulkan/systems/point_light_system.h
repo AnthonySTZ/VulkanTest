07403ff730deb146de152bd0fa159c2d541c108c
AnthonySTZ <anthonycandy2309@gmail.com>
2025-04-01 11:43:05 +0200

    remove appPointer from point light system

--- src/Vulkan/systems/point_light_system.h
+++ src/Vulkan/systems/point_light_system.h
@@ -12,8 +12,6 @@ namespace hdn {
 	class PointLightSystem {
 
 	public:
-		static PointLightSystem* appPointer;
-
 		PointLightSystem(HdnDevice &device, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout);
 		~PointLightSystem();
 
